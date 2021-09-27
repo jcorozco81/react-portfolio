61d7abda84af5101128ef5762a274c4c2d452aee
jcorozco81 <jcorozco@gmail.com>
2021-09-26 22:09:14 -0400

    Added link to resume, added skill list, updated formating

--- src/components/content/Resume.js
+++ src/components/content/Resume.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React from "react";
+import resume from "../../assets/documents/Resume_Juan_Orozco-June-2021.pdf";
 
-
-// React components are isolated parts of an application that help organize and isolate different parts of the program into smaller chunks
-// We can pass data to these components and even render other components inside other components.
-// The `Hello` component is a child of our `App` component and is responsible for rendering the "Hello World" header.
 function Resume() {
-  const message = 'Resume';
-
   return (
     <div className="container">
-      <h2>{message}</h2>
+      <p class="title" id="my-resume">
+        Skills and Resume:
+      </p>
+      <div class="columns">
+
+        <div class="column is-three-fifths has-background-success-light">
+        <div class="columns">
+          <div class="column is-half">
+            <h5 class="title is-5">Front End</h5>
+            <ul>
+              <li>HTML</li>
+              <li>JavaScript, jQuery</li>
+              <li>CSS, CSS Frameworks: Bootstrap, Bulma</li>
+              <li>React</li>
+
+            </ul>
+          </div>
+          <div class="column is-half">
+            <h5 class="title is-5">Back End</h5>
+            <ul>
+              <li>Node.js</li>
+              <li>Express.js</li>
+              <li>Handlebars</li>
+              <li>mySQL</li>
+              <li>MongoDB</li>
+            </ul>
+          </div>
+        </div>
+        </div>
+        <div class="column">
+          <div class="tile is-ancestor">
+            <div class="tile is-parent">
+              <div class="tile is-child box notification is-light has-background-success-light">
+                <h4 class="title is-4">Download my latest resume:</h4>
+                <div class="buttons is-centered">
+                  <a
+                    href={resume}
+                    target="_blank"
+                    rel="noreferrer"
+                    class="button is-info"
+                  >
+                    Download
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
