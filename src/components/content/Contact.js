84bee1e88651305eac978025ee13bd847c02ff69
jcorozco81 <jcorozco@gmail.com>
2021-09-26 22:07:52 -0400

    Updated Contact page, added contact form, updated formating, added validation functions

--- src/components/content/Contact.js
+++ src/components/content/Contact.js
@@ -1,48 +1,129 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { checkEmail } from '../../utils/helper';
-
+import { checkEmail } from "../../utils/helper";
 
 function Contact() {
-  const message = "Contact";
-  const invalidEmailMessage = "Enter a valid email address";
-  const validEmailMessage = "Fill all fields";
+  const [email, setEmail] = useState("");
+  const [fullname, setfullname] = useState("");
+  const [message, setmessage] = useState("");
+  const [errorName, seterrorName] = useState("");
+  const [errorEmail, seterrorEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
+  const invalidEmailMessage = "Enter a valid email address.";
+  const emptyFields = "This field is required.";
+
+  const handleChange = (event) => {
+    const { target } = event;
+    const inputID = target.id;
+    const inputValue = target.value;
+
+    if (inputID === "input-email") {
+      setEmail(inputValue);
+    } else if (inputID === "input-name") {
+      setfullname(inputValue);
+    } else {
+      setmessage(inputValue);
+    }
+  };
+
+  const submitForm = (event) => {
+    event.preventDefault();
+
+    if (!fullname || !checkEmail(email) || !message) {
+      if (!fullname) {
+        seterrorName(emptyFields);
+      } else {
+        seterrorName("");
+      }
+      if (!checkEmail(email)) {
+        seterrorEmail(invalidEmailMessage);
+      } else {
+        seterrorEmail("");
+      }
+      if (!message) {
+        setErrorMessage(emptyFields);
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      alert(
+        `Hello ${fullname}! You Email is ${email} and you message: ${message}`
+      );
+      setfullname("");
+      setEmail("");
+      setmessage("");
+      seterrorName("");
+      seterrorEmail("");
+      setErrorMessage("")
+    }
+  };
 
   return (
     <div className="container">
-      <h2>{message}</h2>
-      <form>
-      <div class="col">
-        <div class="row m-3">
-          <label for="inputName" class="form-label">
-            Name:
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            aria-label="Name"
-          ></input>
-        </div>
+      <p class="title" id="Contact-me">
+        Contact Me:
+      </p>
+      <div class="columns">
+        <div class="column is-two-thirds">
+          <form>
+            <div class="col">
+              <div class="row m-3">
+                <label for="input-name" class="form-label">
+                  Name: <span class="has-text-danger-dark">{errorName}</span>
+                </label>
+                <input
+                  class="input is-info"
+                  type="text"
+                  placeholder="Full Name"
+                  id="input-name"
+                  value={fullname}
+                  onChange={handleChange}
+                />
+              </div>
 
-        <div class="row m-3">
-          <label for="inputEmail" class="form-label">
-            Email:
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="inputEmail"
-          ></input>
-        </div>
-        <div class="row m-3">
-          <label for="w3review" class="form-label">Message:</label>
-          <textarea id="message-area" class="form-control" name="message-area" rows="4" cols="50">
-          </textarea>
+              <div class="row m-3">
+                <label for="input-email" class="form-label">
+                  Email: <span class="has-text-danger-dark">{errorEmail}</span>
+                </label>
+                <input
+                  class="input is-info"
+                  type="email"
+                  placeholder="Email"
+                  id="input-email"
+                  value={email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div class="row m-3">
+                <label for="input-message" class="form-label">
+                  Message:{" "}
+                  <span class="has-text-danger-dark">{errorMessage}</span>
+                </label>
+                <textarea
+                  class="textarea is-info"
+                  placeholder="Enter you message"
+                  id="input-message"
+                  value={message}
+                  onChange={handleChange}
+                />
+              </div>
+              <div class="row m-3">
+                <div class="buttons">
+                  <button
+                    class="button is-info"
+                    id="submit"
+                    onClick={submitForm}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
+          <div class="row m-3"></div>
         </div>
       </div>
-      </form>
-      <h2></h2>
     </div>
   );
 }
