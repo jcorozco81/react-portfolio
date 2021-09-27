443baf0d9b0fb08d2c0d5c110d85dc196129c5b6
jcorozco81 <jcorozco@gmail.com>
2021-09-26 22:07:15 -0400

    Updated About page, added personal info, updated formating

--- src/components/content/About.js
+++ src/components/content/About.js
@@ -11,25 +11,17 @@ function About() {
   return (
 
 
-    <div class="column is-four-fifths">
+    <div class="container">
 
 
 
 
 
-
-      <div class="tile is-ancestor">
-        <div class="tile is-parent">
-          <div class="tile is-child box notification is-warning">
-            <p class="title" id="about-me">About Me:</p>
-          </div>
-        </div>
-      </div>
 
 
       <div class="tile is-ancestor">
         <div class="tile is-parent is-4">
-          <div class="tile is-child box notification is-light has-background-primary-dark">
+          <div class="tile is-child box notification is-light">
             <div class="block">
               <figure class="image">
                 <img alt="Juan Orozco" src={ProfilePic}></img>
@@ -39,16 +31,9 @@ function About() {
         </div>
 
         <div class="tile is-parent">
-          <div class="tile is-child box notification is-primary has-background-primary-dark">
-            <p class="is-size-3-desktop is-size-6-mobile is-size-5-tablet">Hello, my name is Juan Carlos Orozco. I was
-              born in Puerto Rico. I have a Bachelor Degree in Electrical Engineering and a Master in Business
-              Administration. I'm currently enrolled in a Coding Bootcamp working to learn the skills to become a Web
-              Developer. In this page you will find samples of my work. If you see something interesting don't hesitate
-              to contact me. My contact information is at the bottom of this page. Enjoy!</p>
-            <div class="buttons is-centered mt-5">
-              <a href="/assets/documents/Resume_Juan_Orozco-June-2021.pdf" class="button is-large is-danger">My
-                Resume</a>
-            </div>
+          <div class="tile is-child box notification">
+          <p class="title" id="about-me">About Me:</p>
+            <p class="is-size-5-desktop is-size-7-mobile is-size-6-tablet">{message}</p>
           </div>
         </div>
       </div>
