60e640ba4c1db48153c082cf1be0bb43c7337f5b
jcorozco81 <jcorozco@gmail.com>
2021-09-26 22:05:14 -0400

    Updated Footer, updated links and formating

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,92 @@
-import React from 'react';
+import React from "react";
+import svgEmail from "../assets/svg/email.svg";
+import svgLinkedIn from "../assets/svg/linkedin.svg";
+import svgGit from "../assets/svg/github.svg";
 
 function Footer() {
   // const message = 'Hello world! I am a React Component';
 
   return (
     <div className="container">
-      {/* <h2>{message}</h2> */}
-      <ul class="nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="github" onClick = {() => window.open('https://github.com/jcorozco81')}>
-            GitHub
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="linkedin" onClick = {() => window.open('https://www.linkedin.com/in/juan-carlos-orozco-b6276434/')}>
-            LinkedIn
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="Email" onClick = {() => window.open('mailto:jcorozco@gmail.com')}>
-            Email
-          </a>
-        </li>
-      </ul>
+        <div class="column has-text-centered">
+          <div class="column">
+            <div class="tile is-ancestor has-text-centered">
+              <div class="tile is-parent">
+                <article class="tile is-child">
+                  <img alt="Juan Orozco" src={svgGit}></img>
+                  <a
+                    class="nav-link active"
+                    aria-current="page"
+                    href="github"
+                    onClick={() => window.open("https://github.com/jcorozco81")}
+                  >
+                    GitHub
+                  </a>
+                </article>
+              </div>
+
+              <div class="tile is-parent">
+                <article class="tile is-child">
+                  <img alt="Juan Orozco" src={svgLinkedIn}></img>
+                  <a
+                    class="nav-link"
+                    href="linkedin"
+                    onClick={() =>
+                      window.open(
+                        "https://www.linkedin.com/in/juan-carlos-orozco-b6276434/"
+                      )
+                    }
+                  >
+                    LinkedIn
+                  </a>
+                </article>
+              </div>
+
+              <div class="tile is-parent">
+                <article class="tile is-child">
+                  <img alt="Juan Orozco" src={svgEmail}></img>
+                  <a
+                    class="nav-link"
+                    href="Email"
+                    onClick={() => window.open("mailto:jcorozco@gmail.com")}
+                  >
+                    Email
+                  </a>
+                </article>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <p>© 2021 Created by Juan Orozco</p>
+          </div>
+        </div>
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
 
 export default Footer;
+// Logos from https://iconmonstr.com/
