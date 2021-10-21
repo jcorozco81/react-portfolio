9d6956206e47cf4359a44d3309d279f22d1d5427
jcorozco81 <jcorozco@gmail.com>
2021-10-21 02:04:41 -0400

    Created express and apollo server, added mongoDB connection

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const db = require("./config/connection");
+const { typeDefs, resolvers } = require("./schemas");
+const http = require("http");
+
+const PORT = process.env.PORT || 4001;
+
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
+  
+  await new Promise((resolve) => httpServer.listen(PORT, resolve));
+  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+db.once('open', () => startApolloServer(typeDefs, resolvers));
