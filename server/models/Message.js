271b1cb30fae0a518623f964b4dcec9430d82aed
jcorozco81 <jcorozco@gmail.com>
2021-10-21 02:02:19 -0400

    Created moongose model files for the messages

---
+++ server/models/Message.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const messageSchema = new Schema({
+
+name:{
+    type: String,
+    required: true,
+    trim: true,
+},
+email:{
+    type: String,
+    required: true,
+    trim: true,
+},
+body:{
+    type: String,
+    required: true,
+    trim: true,
+},
+
+});
+
+const Message = model('Message', messageSchema);
+
+module.exports = Message;
---
+++ server/models/index.js
@@ -0,0 +1,3 @@
+const Message = require('./Message');
+
+module.exports = { Message };
